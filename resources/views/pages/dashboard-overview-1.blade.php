94e764603773c89922608f54addad852e8800991
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-04-29 20:22:31 +0100

    wip

--- resources/views/pages/dashboard-overview-1.blade.php
+++ resources/views/pages/dashboard-overview-1.blade.php
@@ -11,7 +11,7 @@
                 <!-- BEGIN: General Report -->
                 <div class="col-span-12 mt-8">
                     <div class="intro-y flex items-center h-10">
-                        <h2 class="text-lg font-medium truncate mr-5">General Report</h2>
+                        <h2 class="text-lg font-medium truncate mr-5">As E dey go</h2>
                         <a href="" class="ml-auto flex text-theme-1 dark:text-theme-10">
                             <i data-feather="refresh-ccw" class="w-4 h-4 mr-3"></i> Reload Data
                         </a>
