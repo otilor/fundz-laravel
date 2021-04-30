3bcaa550fc4398d503de69f31840ae31c346d318
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-04-30 20:22:31 +0100

    wip

--- resources/views/pages/savings.blade.php
+++ resources/views/pages/savings.blade.php
@@ -6,7 +6,7 @@
 
 @section('subcontent')
     <div class="intro-y flex items-center mt-8">
-        <h2 class="text-lg font-medium mr-auto">Form Layout</h2>
+        <h2 class="text-lg font-medium mr-auto">Deposit money 🤑</h2>
     </div>
     <div class="grid grid-cols-12 gap-6 mt-5">
         <div class="intro-y col-span-12 lg:col-span-6">
@@ -77,5 +77,5 @@
             </div>
             <!-- END: Form Layout -->
         </div>
-    </div>    
+    </div>
 @endsection
