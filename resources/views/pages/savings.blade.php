d3e8cde10c85c6717c795fdc644d5225e34cca86
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-04-30 20:32:47 +0100

    wip

--- resources/views/pages/savings.blade.php
+++ resources/views/pages/savings.blade.php
@@ -8,22 +8,25 @@
     <div class="intro-y flex items-center mt-8">
         <h2 class="text-lg font-medium mr-auto">Deposit money 🤑</h2>
     </div>
-    <div class="grid grid-cols-12 gap-6 mt-5">
-        <div class="intro-y col-span-12 lg:col-span-6">
-            <!-- BEGIN: Form Layout -->
-            <div class="intro-y box p-5">
-                <div>
-                    <label for="crud-form-1" class="form-label">Amount</label>
-                    <div class="input-group">
-                        <div id="input-group-3" class="input-group-text">₦</div>
-                        <input id="crud-form-1" type="text" class="form-control w-full" placeholder="Example 2000">
+    <form>
+        <div class="grid grid-cols-12 gap-6 mt-5">
+            <div class="intro-y col-span-12 lg:col-span-6">
+                <!-- BEGIN: Form Layout -->
+                <div class="intro-y box p-5">
+                    <div>
+                        <label for="crud-form-1" class="form-label">Amount</label>
+                        <div class="input-group">
+                            <div id="input-group-3" class="input-group-text">₦</div>
+                            <input id="crud-form-1" type="text" class="form-control w-full" placeholder="Example 2000">
+                        </div>
+                    </div>
+                    <div class="text-right mt-5">
+                        <button type="submit" class="btn btn-primary w-24">Save</button>
                     </div>
                 </div>
-                <div class="text-right mt-5">
-                    <button type="button" class="btn btn-primary w-24">Save</button>
-                </div>
+                <!-- END: Form Layout -->
             </div>
-            <!-- END: Form Layout -->
         </div>
-    </div>
+    </form>
+
 @endsection
