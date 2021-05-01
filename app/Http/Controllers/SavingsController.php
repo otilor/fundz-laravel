7bf0fa10bd8b2de7c809e5a7a32dffe8ea2811f2
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-05-01 16:25:25 +0100

    Declare callback method

--- app/Http/Controllers/SavingsController.php
+++ app/Http/Controllers/SavingsController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Request\SaveMoneyRequest;
 use App\Repositories\UserRepository;
+use KingFlamez\Rave\Facades\Rave as Flutterwave;
 
 class SavingsController extends Controller
 {
@@ -22,5 +23,23 @@ class SavingsController extends Controller
     {
         $this->user->topupWallet(userId: auth()->id(), amount: $request->amount);
         return redirect(route('dashboard-overview-1'));
+    }
+
+    public function callback()
+    {
+        $transactionID = Flutterwave::getTransactionIDFromCallback();
+        $data = Flutterwave::verifyTransaction($transactionID);
+
+        dd($data);
+        // Get the transaction from your DB using the transaction reference (txref)
+        // Check if you have previously given value for the transaction. If you have, redirect to your successpage else, continue
+        // Confirm that the $data['data']['status'] is 'successful'
+        // Confirm that the currency on your db transaction is equal to the returned currency
+        // Confirm that the db transaction amount is equal to the returned amount
+        // Update the db transaction record (including parameters that didn't exist before the transaction is completed. for audit purpose)
+        // Give value for the transaction
+        // Update the transaction to note that you have given value for the transaction
+        // You can also redirect to your success page from here
+
     }
 }
