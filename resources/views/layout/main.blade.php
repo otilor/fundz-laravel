5bc4b1f87429b098ea5b67c8020c1eb41a7fafbd
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-05-02 13:28:39 +0100

    Display notification and bottom right

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,9 +1,12 @@
 @extends('../layout/base')
-
 @section('body')
-    <body class="main font-karla">
-        @yield('content')
 
+    <body class="main font-karla">
+
+    <div class="absolute bottom-0 right-0 h-16 w-16 ...">
+        <x:notify-messages />
+    </div>
+        @yield('content')
         <!-- BEGIN: JS Assets-->
         <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
         <script src="https://maps.googleapis.com/maps/api/js?key=["your-google-map-api"]&libraries=places"></script>
