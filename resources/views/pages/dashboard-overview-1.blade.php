68b3d07864097785146178b24bfdfd6ba848fe03
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-05-03 16:42:10 +0100

    wip

--- resources/views/pages/dashboard-overview-1.blade.php
+++ resources/views/pages/dashboard-overview-1.blade.php
@@ -13,7 +13,7 @@
             <div class="grid grid-cols-12 gap-6">
                 <!-- BEGIN: General Report -->
                 <div class="col-span-12 mt-8">
-                    <h1 class="text-5xl font-bold truncate mr-6">{{ auth()->user()->name }}</h1>
+                    <h1 class="text-3xl font-bold truncate mr-6">{{ auth()->user()->name }}</h1>
                     <div class="intro-y flex items-center h-10">
                         <h2 class="text-lg font-medium truncate mr-5">Hello, wash your hands regularly🧼🧴</h2>
                         <a href="" class="ml-auto flex text-theme-1 dark:text-theme-10">
