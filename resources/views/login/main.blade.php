3c14f72575eae150a1d0eec0e943975cd9f09d30
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-05-03 14:47:06 +0100

    wip

--- resources/views/login/main.blade.php
+++ resources/views/login/main.blade.php
@@ -12,13 +12,13 @@
                 <a href="" class="-intro-x flex items-center pt-5">
                     <img alt="Rubick Tailwind HTML Admin Template" class="w-6" src="{{ asset('dist/images/logo.svg') }}">
                     <span class="text-white text-lg ml-3">
-                        Ru<span class="font-medium">bick</span>
+                        Fu<span class="font-medium">ndz</span>
                     </span>
                 </a>
                 <div class="my-auto">
                     <img alt="Rubick Tailwind HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="{{ asset('dist/images/illustration.svg') }}">
                     <div class="-intro-x text-white font-medium text-4xl leading-tight mt-10">A few more clicks to <br> sign in to your account.</div>
-                    <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-gray-500">Manage all your e-commerce accounts in one place</div>
+                    <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-gray-500">Manage all your fundz in one place</div>
                 </div>
             </div>
             <!-- END: Login Info -->
