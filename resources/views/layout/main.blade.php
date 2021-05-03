cb5a230427a78e0e5e891d120ab0e86c21b66838
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-05-03 16:05:33 +0100

    wip

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -2,7 +2,15 @@
 @section('body')
 
     <body class="main font-karla">
-
+    <div id="success-notification-content" class="toastify-content hidden flex"><i class="text-theme-9"
+                                                                                   data-feather="check-circle"></i>
+        <div class="ml-4 mr-4">
+            <div class="font-medium">Payment successful!</div>
+            <div class="text-gray-600 mt-1">Your account has been credited!🎉</div>
+        </div>
+    </div> <!-- END: Notification Content --> <!-- BEGIN: Notification Toggle -->
+    <button style="display: none" type="hidden" name="success-alert" id="success-notification-toggle" class="btn btn-primary">Show Notification
+    </button> <!-- END: Notification Toggle -->
 
         @yield('content')
         <!-- BEGIN: JS Assets-->
--- resources/views/pages/dashboard-overview-1.blade.php
+++ resources/views/pages/dashboard-overview-1.blade.php
@@ -6,15 +6,6 @@
 
 @section('subcontent')
     <!-- BEGIN: Notification Content -->
-    <div id="success-notification-content" class="toastify-content hidden flex"><i class="text-theme-9"
-                                                                                   data-feather="check-circle"></i>
-        <div class="ml-4 mr-4">
-            <div class="font-medium">Payment successful!</div>
-            <div class="text-gray-600 mt-1">Your account has been credited!🎉</div>
-        </div>
-    </div> <!-- END: Notification Content --> <!-- BEGIN: Notification Toggle -->
-    <button style="display: none" type="hidden" name="success-alert" id="success-notification-toggle" class="btn btn-primary">Show Notification
-    </button> <!-- END: Notification Toggle -->
 
 
     <div class="grid grid-cols-12 gap-6">
