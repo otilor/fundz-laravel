a33f8e338b806e1ee890a78def93e3162e7f946c
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-05-04 09:33:00 +0100

    Handle route with flutterwave controller

---
+++ app/Http/Controllers/FlutterwaveController.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class FlutterwaveController extends Controller
+{
+    public function webhook(Request $request)
+    {
+        //This verifies the webhook is sent from Flutterwave
+        $verified = Flutterwave::verifyWebhook();
+
+        // if it is a charge event, verify and confirm it is a successful transaction
+        if ($verified && $request->event == 'charge.completed' && $request->data->status == 'successful') {
+            $verificationData = Flutterwave::verifyPayment($request->data['id']);
+            if ($verificationData['status'] === 'success') {
+                // process for successful charge
+
+            }
+
+        }
+
+        // if it is a transfer event, verify and confirm it is a successful transfer
+        if ($verified && $request->event == 'transfer.completed') {
+
+            $transfer = Flutterwave::transfers()->fetch($request->data['id']);
+
+            if ($transfer['data']['status'] === 'SUCCESSFUL') {
+                // update transfer status to successful in your db
+            } else if ($transfer['data']['status'] === 'FAILED') {
+                // update transfer status to failed in your db
+                // revert customer balance back
+            } else if ($transfer['data']['status'] === 'PENDING') {
+                // update transfer status to pending in your db
+            }
+
+        }
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FlutterwaveController;
 use App\Http\Controllers\SavingsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
