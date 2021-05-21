5ba65564872765f37a0a22adaa07b729d8e4ee82
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-05-21 19:52:33 +0100

    wip

--- app/Http/Requests/WithdrawRequest.php
+++ app/Http/Requests/WithdrawRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Password;
 
 class WithdrawRequest extends FormRequest
 {
@@ -24,7 +25,11 @@ class WithdrawRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'amount' => 'required',
+            'bank_name' => 'required',
+            'account_number' => 'required',
+            'comment' => 'nullable',
+            'password' => \Illuminate\Validation\Rules\Password::min(8),
         ];
     }
 }
