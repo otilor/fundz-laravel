576f721de7d1d1f0373f9a4f39937c1b4c30e973
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-05-21 10:33:28 +0100

    Bump composer.lock

--- resources/views/pages/dashboard-overview-1.blade.php
+++ resources/views/pages/dashboard-overview-1.blade.php
@@ -15,7 +15,7 @@
                 <div class="col-span-12 mt-8">
                     <h1 class="text-3xl font-bold truncate mr-6">{{ auth()->user()->name }}</h1>
                     <div class="intro-y flex items-center h-10">
-                        <h2 class="text-lg font-medium truncate mr-5">Hello, wash your hands regularly🧼🧴</h2>
+                        <h2 class="text-lg font-medium truncate mr-5">Hello, wash your hands regularly</h2>
                         <a href="" class="ml-auto flex text-theme-1 dark:text-theme-10">
                             <i data-feather="refresh-ccw" class="w-4 h-4 mr-3"></i> Reload Data
                         </a>
