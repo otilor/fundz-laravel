2210fc90e53b6b115950b1476ee5e6ed4ec2db85
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-05-23 18:28:50 +0100

    Create a safelock

--- app/Http/Controllers/SafelockController.php
+++ app/Http/Controllers/SafelockController.php
@@ -4,10 +4,30 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Requests\CreateSafeolockRequest;
+use Illuminate\Support\Str;
+
 class SafelockController
 {
     public function index()
     {
         return view('pages.safelock');
+    }
+
+
+    public function create()
+    {
+        return view('pages.create');
+    }
+
+    public function store(CreateSafeolockRequest $request)
+    {
+        auth()->user()->createWallet([
+            'name' => $request->wallet_name,
+            'slug' => Str::slug('name')
+        ]);
+
+        session()->flash('success', 'Created new safelock');
+        return back();
     }
 }
