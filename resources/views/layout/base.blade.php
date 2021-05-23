ea38525ebd54871a96eb9c17b8785ea6308b2850
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-05-23 20:02:00 +0100

    wip

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -15,7 +15,7 @@ License: You must have a valid license purchased only from themeforest(the above
     <link href="{{ asset('dist/images/logo.svg') }}" rel="shortcut icon">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Rubick admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
+    <meta name="description" content="Fundz by Gabriel">
     <meta name="keywords" content="admin template, Rubick admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="LEFT4CODE">
 
