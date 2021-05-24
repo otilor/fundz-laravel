ac30699574b926b9d5080bee26b071c889d8a84d
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-05-24 06:10:50 +0100

    wip

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -76,7 +76,7 @@
                             </div>
                             <div class="col-span-12 xl:col-span-6">
                                 <div class="h-40 relative image-fit cursor-pointer zoom-in mx-auto">
-                                    <img class="rounded-md" alt="{{$user->name. ' picture'}}" src="{{asset('storage/image/'.$user->photo) ?? 'dist/images/profile-15.jpg'}}">
+                                    <img class="rounded-md" alt="{{$user->name. ' picture'}}" src="{{asset('storage/image/'.auth()->user()->photo) ?? 'dist/images/profile-15.jpg'}}">
                                     <div class="tooltip w-5 h-5 flex items-center justify-center absolute rounded-full text-white bg-theme-6 right-0 top-0 -mr-2 -mt-2"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x w-4 h-4"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg> </div>
                                 </div>
                                 <div class="mx-auto cursor-pointer relative mt-5">
