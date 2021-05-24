cc6434f47fb1b0845aa8ef76f0b0e9a15f3aafea
Akiode Timothy <akiodetimothy2017@gmail.com>
2021-05-24 15:17:40 +0100

    Fix Sign in issue

--- resources/views/register/main.blade.php
+++ resources/views/register/main.blade.php
@@ -69,7 +69,7 @@
             </div>
             <div class="intro-x mt-5 xl:mt-8 text-center xl:text-left">
                 <button class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top" type="submit">Register</button>
-                <button class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top">Sign in</button>
+                <a href="/login" class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top">Sign In</a>
             </div>
         </div>
     </div>
