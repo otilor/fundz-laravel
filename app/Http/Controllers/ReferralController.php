351e27aa71f582a62887fe93305dd01cb12ac98c
Akiode Timothy <akiodetimothy2017@gmail.com>
2021-06-02 16:16:33 +0100

    Refactor ReferralController

--- app/Http/Controllers/ReferralController.php
+++ app/Http/Controllers/ReferralController.php
@@ -13,13 +13,13 @@ class ReferralController extends Controller
     }
     public function index()
     {
-        $referrals = User::select('name','email','affiliate_id','created_at','paid')->whereReferred_by(auth()->user()->affiliate_id)->orderBy('created_at','desc')->get();
+        $referrals = $this->user->getUserReferrals(auth()->user()->affiliate_id)['referrals'];
         return view('pages.Referral.index', compact('referrals'));
     }
 
     public function RequestPayment($affiliateId)
     {
-        $ReferredUserDetails = User::whereAffiliate_id($affiliateId)->first();
+        $ReferredUserDetails = $this->user->getReferredUserDetails($affiliateId)['details'];
         if($ReferredUserDetails->email_verified_at == null)
         {
             return redirect()->back()->with('error', 'The User you referred has not been verified.');
@@ -27,17 +27,13 @@ class ReferralController extends Controller
         else
         {
             $this->user->topupWallet(amount: 1000, userId: auth()->id());
-            $payReferrar = User::where('affiliate_id',$ReferredUserDetails->referred_by)->update([
-                'referral_earning' => auth()->user()->referral_earning + 1000,
-            ]);
-            if($payReferrar){
-                User::where('affiliate_id',$affiliateId)->update([
-                    'paid' => true,
-                ]);
-                return redirect()->back()->with('success','You have be credited 1000 🤩 🥳 🥳');
+            $payUser = $this->user->payReferrar($ReferredUserDetails, $affiliateId);
+            if($payUser['status'] == true)
+            {
+                return redirect()->back()->with('error', 'You have be credited 1000 🤩 🥳 🥳');
             }
-            else{
-                return redirect()->back()->with('error','There was an issue while processiing your payment. Try again in 5 minutes');
+            else {
+                return redirect()->back()->with('error', 'There was an issue while processiing your payment. Try again in 5 minutes');
             }
         }
     }
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -26,6 +26,10 @@ class UserService implements UserRepository
         return ['message' => 'Successfully topped up wallet', 'status' => true];
     }
 
+    public function getUserReferrals($affiliate_id){
+        $referrals = User::select('name','email','affiliate_id','created_at','paid')->whereReferred_by($affiliate_id)->orderBy('created_at','desc')->get();
+        return ['referrals' => $referrals,'status' => true];
+    }
     public function withdraw($amount, $userId)
     {
         $balance = $this->getWalletBalance($userId)['balance'];
@@ -49,5 +53,28 @@ class UserService implements UserRepository
 
         User::find($userId)->withdraw($amount);
         return true;
+    }
+
+    public function payReferrar($ReferredUserDetails,$affiliateId)
+    {
+        $payReferrar = User::where('affiliate_id',$ReferredUserDetails->referred_by)->update([
+            'referral_earning' => auth()->user()->referral_earning + 1000,
+        ]);
+        if($payReferrar){
+            User::where('affiliate_id',$affiliateId)->update([
+                'paid' => true,
+            ]);
+            return ['message' => 'You have be credited 1000 🤩 🥳 🥳', 'status' => true];
+        }
+        else{
+            return ['message' => 'There was an issue while processiing your payment. Try again in 5 minutes', 'status' => false];
+
+        }
+    }
+
+    public function getReferredUserDetails($affiliateId)
+    {
+        $ReferredUserDetails = User::whereAffiliate_id($affiliateId)->first();
+        return ['details' => $ReferredUserDetails];
     }
 }
