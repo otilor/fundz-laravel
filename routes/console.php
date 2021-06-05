6f4a193c564dee6ac6ac7f656418a9b00097c3ad
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-06-05 18:05:48 +0100

    Add comma

--- routes/console.php
+++ routes/console.php
@@ -18,4 +18,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Artisan::clearResolvedInstances()
+Artisan::clearResolvedInstances();
