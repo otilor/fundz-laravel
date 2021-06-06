24d05aa26de8fce12da27206c6a92ae1532e860e
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-06-06 15:08:37 +0100

    Add new column to user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'payment_hash'=> \Str::slug(\Str::random(8)),
             'gender' => ['male', 'female'][rand(0, 1)],
             'active' => 1,
             'remember_token' => Str::random(10),
