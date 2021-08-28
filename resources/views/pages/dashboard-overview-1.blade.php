7c9913cac6c8582ebd6dedfbbeb572155da99f8e
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-08-28 18:14:28 +0100

    Update notification

--- resources/views/pages/dashboard-overview-1.blade.php
+++ resources/views/pages/dashboard-overview-1.blade.php
@@ -19,7 +19,7 @@
                         </a>
                     </div>
                     <div class="alert alert-dismissible show box bg-theme-3 text-white flex items-center mb-6" role="alert">
-                        <span>Introducing named wallets! Personalize your wallets.</span>
+                        <span>A new feature is coming in the next couple of days!🎁🎁🎁</span>
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x w-4 h-4"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
                         </button>
