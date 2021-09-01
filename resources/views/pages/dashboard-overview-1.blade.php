f46367dca9b765f175f877b098d70c8a6040ff35
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-09-01 21:19:49 +0100

    Remove headings

--- resources/views/pages/dashboard-overview-1.blade.php
+++ resources/views/pages/dashboard-overview-1.blade.php
@@ -11,9 +11,8 @@
             <div class="grid grid-cols-12 gap-6">
                 <!-- BEGIN: General Report -->
                 <div class="col-span-12 mt-8">
-                    <h1 class="text-3xl font-bold truncate mr-6">{{ auth()->user()->name }}</h1>
                     <div class="intro-y flex items-center h-10">
-                        <h2 class="text-lg font-medium truncate mr-5">Hello, wash your hands regularly</h2>
+                        <h2 class="text-lg font-medium truncate mr-5">Welcome, {{ auth()->user()->name }}</h2>
                         <a href="" class="ml-auto flex text-theme-1 dark:text-theme-10">
                             <i data-feather="refresh-ccw" class="w-4 h-4 mr-3"></i> Reload Data
                         </a>
